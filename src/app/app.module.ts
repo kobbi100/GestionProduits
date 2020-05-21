245a703538b386baab46b538bb77a4610de2a365
kobbi100 <aminekobb77@gmail.com>
2020-05-21 03:34:24 +0100

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProduitComponent } from './produit/produit.component';
+import {ProduitMockService} from './produit/produit.mock.service';
+import {Produit} from '../shared/produit';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProduitComponent,
+    Produit
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ProduitMockService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
